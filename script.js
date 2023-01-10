30aa7f15eb7b6c7904b47e669545885f2df1bb4e
ierdna89 <andreistrelciuc@gmail.com>
2023-01-10 20:57:41 +0200

    Adde javascript to validate password

--- script.js
+++ script.js
@@ -2,22 +2,58 @@ const listenEnterPassword = document.getElementById("password");
 const listenRepeatPassword = document.getElementById("confirm-password");
 const errorText = document.getElementById("error-text");
 const createAccountButton = document.getElementById("create-account");
+const signupForm = document.getElementById("subscribeform");
 
-function checkPassword() {
+// function checkPassword() {
 
-	const enterPassword = document.getElementById("password").value;
-	const repeatPassword = document.getElementById("confirm-password").value;
+// 	const enterPassword = document.getElementById("password").value;
+// 	const repeatPassword = document.getElementById("confirm-password").value;
 
-	if (enterPassword != repeatPassword) {
-		errorText.innerHTML = "*Passwords don't match";
-	}
+// 	if (enterPassword != repeatPassword) {
+// 		errorText.innerHTML = "*Passwords don't match";
+// 		preventSubmit():
+// 	}
+// 	else {
+// 		errorText.innerHTML = "";
+// 	}
+// }
 
-	else {
-		errorText.innerHTML = "";
-	}
-}
+// // listenEnterPassword.onkeyup = function() {checkPassword()};
+// // listenRepeatPassword.onkeyup = function() {checkPassword()};
+// createAccountButton.onclick = function() {checkPassword()};
 
 
-// listenEnterPassword.onkeyup = function() {checkPassword()};
-// listenRepeatPassword.onkeyup = function() {checkPassword()};
-createAccountButton.onclick = function() {checkPassword()};
+
+
+// function preventSubmit() {
+// 	signupForm.addEventListener("submit", 
+// 		function(event) {
+// 			event.preventDefault();
+// 		}
+// 	);
+// }
+
+
+
+
+createAccountButton.addEventListener("click", (e) =>
+	{	
+
+		const enterPassword = document.getElementById("password").value;
+		const repeatPassword = document.getElementById("confirm-password").value;
+		
+		if (enterPassword != repeatPassword) {
+			errorText.innerHTML = "*Passwords don't match";
+			listenEnterPassword.style.borderColor = "red";
+			listenRepeatPassword.style.borderColor = "red";
+			console.log("click");
+			event.preventDefault();
+		}
+		else {
+			console.log("poc");
+			listenEnterPassword.style.borderColor = "greeen";
+			listenRepeatPassword.style.borderColor = "green";
+			errorText.innerHTML = "";
+		}
+  	
+});
